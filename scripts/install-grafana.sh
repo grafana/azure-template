479a81064e31de2b2587f4f0fd9fd80f22b6f9e9
Jacob Lisi <jacob.t.lisi@gmail.com>
2017-10-19 13:17:10 -0400

    update install script

---
+++ scripts/install-grafana.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://s3-us-west-2.amazonaws.com/grafana-releases/release/grafana_4.5.2_amd64.deb
+sudo apt-get install -y adduser libfontconfig
+sudo dpkg -i grafana_4.5.2_amd64.deb
+systemctl daemon-reload
+systemctl start grafana-server
+sudo systemctl enable grafana-server.service
