21952faad0f0c2e628e13187925ac4855284b4ca
Jacob Lisi <jacob.t.lisi@gmail.com>
2017-10-20 15:20:33 -0400

    update install script

--- scripts/install-grafana.sh
+++ scripts/install-grafana.sh
@@ -6,7 +6,35 @@ log()
     echo "`date -u +'%Y-%m-%d %H:%M:%S'`: ${1}"
 }
 
+help()
+{
+    echo "This script installs Grafana cluster on Ubuntu"
+    echo "Parameters:"
+    echo "-A admin password"
+    echo "-h view this help content"
+}
+
 # Parameters
+ADMIN_PWD="admin"
+
+#Loop through options passed
+while getopts :n:v:A:R:K:S:Z:p:a:k:L:C:Xxyzldjh optname; do
+  log "Option $optname set"
+  case $optname in
+    A)
+      ADMIN_PWD="${OPTARG}"
+    h) #show help
+      help
+      exit 2
+      ;;
+    \?) #unrecognized option - show help
+      echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
+      help
+      exit 2
+      ;;
+  esac
+done
+
 
 # Command Line Opts
 
@@ -26,5 +54,20 @@ start_grafana()
     sudo systemctl enable grafana-server.service
 }
 
+# Install the Azure Monitor Datasource
+install_plugins()
+{
+    grafana-cli plugins install grafana-azure-monitor-datasource
+    systemctl restart grafana-server
+}
+
+# Update the grafana passord of the admin account
+configure_admin_password()
+{   
+    grafana-cli admin reset-admin-password ${ADMIN_PWD}
+}
+
 install_grafana
 start_grafana
+configure_admin_password
+install_plugins
